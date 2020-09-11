225242acc24a9275eafbd162e3c34efbc73e9dbb
Yulun Tian <yulun@mit.edu>
2020-09-10 22:14:58 -0400

    add gtest

--- code/C++/DPGO/tests/testOptimizationThread.cpp
+++ code/C++/DPGO/tests/testOptimizationThread.cpp
@@ -1,46 +1,30 @@
-/* ----------------------------------------------------------------------------
- * Copyright 2020, Massachusetts Institute of Technology, * Cambridge, MA 02139
- * All Rights Reserved
- * Authors: Yulun Tian, et al. (see README for the full author list)
- * See LICENSE for the license information
- * -------------------------------------------------------------------------- */
-
-#include <iostream>
-#include <cassert>
+#include "gtest/gtest.h"
 #include "QuadraticProblem.h"
 #include "distributed/PGOAgent.h"
 #include "DPGO_types.h"
 #include "DPGO_utils.h"
 
-using namespace std;
 using namespace DPGO;
 
-int main(int argc, char** argv)
+TEST(testDPGO, OptimizationThread)
 {
-    
-    cout << "Testing optimization thread of PGOAgent..." << endl;
-    
     unsigned int d,r;
     d = 3;
     r = 5;
     ROPTALG algorithm = ROPTALG::RTR;
     bool verbose = false;
-    PGOAgentParameters options(d,r,algorithm,verbose);
+    bool online = false;
+    PGOAgentParameters options(d,r,algorithm,verbose,online);
 
     PGOAgent agent(0, options);
 
+    ASSERT_FALSE(agent.isOptimizationRunning());
 
-    assert(agent.isOptimizationRunning() == false);
-
-    for(unsigned trial = 0; trial < 5; ++trial){
+    for(unsigned trial = 0; trial < 3; ++trial){
     	agent.startOptimizationLoop(1.0);
-	    sleep(5);
-	    assert(agent.isOptimizationRunning() == true);
+	    sleep(1);
+	    ASSERT_TRUE(agent.isOptimizationRunning());
 	    agent.endOptimizationLoop();
-	    assert(agent.isOptimizationRunning() == false);
+	    ASSERT_FALSE(agent.isOptimizationRunning());
     }
-
-    cout << "Passed." << endl;
-
-    exit(0);
 }
