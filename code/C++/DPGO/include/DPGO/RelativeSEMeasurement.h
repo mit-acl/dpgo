2ff63f559668cb15deb3985c856a1e47ff4091e4
Yulun Tian <yulun@mit.edu>
2021-03-12 23:53:41 -0500

    Add known inlier flag for measurement

--- code/C++/DPGO/include/DPGO/RelativeSEMeasurement.h
+++ code/C++/DPGO/include/DPGO/RelativeSEMeasurement.h
@@ -43,6 +43,9 @@ struct RelativeSEMeasurement {
   /** Translational measurement precision */
   double tau;
 
+  /** If this measurement is an known inlier */
+  bool isKnownInlier;
+
   /** Weight between (0,1) used in Graduated Non-Convexity */
   double weight;
 
@@ -64,9 +67,10 @@ struct RelativeSEMeasurement {
         t(relative_translation),
         kappa(rotational_precision),
         tau(translational_precision),
+        isKnownInlier(false),
         weight(1.0) {}
 
-  /** A utility function for streaming Nodes to cout */
+  /** A utility function for streaming this struct to cout */
   inline friend std::ostream &operator<<(
       std::ostream &os, const RelativeSEMeasurement &measurement) {
     os << "r1: " << measurement.r1 << std::endl;
@@ -77,6 +81,7 @@ struct RelativeSEMeasurement {
     os << "t: " << std::endl << measurement.t << std::endl;
     os << "Kappa: " << measurement.kappa << std::endl;
     os << "Tau: " << measurement.tau << std::endl;
+    os << "Is known inlier: " << measurement.isKnownInlier << std::endl;
     os << "Weight: " << measurement.weight << std::endl;
 
     return os;
